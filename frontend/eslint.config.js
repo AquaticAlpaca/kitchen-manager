198aeb2548f7e903bf0e827fb21f0d5020bf05c9
Aquatic Alpaca <185613589+AquaticAlpaca@users.noreply.github.com>
2026-07-13 22:38:09 +0200

    Load the testing library correctly

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const testingLibraryPlugin = require("eslint-plugin-testing-library");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
     files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    extends: ["plugin:testing-library/react"],
+    plugins: {
+      "testing-library": testingLibraryPlugin,
+    },
+    rules: {
+      // Add testing-library recommended rules here
+      ...testingLibraryPlugin.configs.react.rules,
+    },
   },
 ]);
